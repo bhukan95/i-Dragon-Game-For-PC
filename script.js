10815addbf9560184ea6162fe2a4e3824fb83bfe
Prakash Bhukan <120048409+bhukan95@users.noreply.github.com>
2024-08-05 10:49:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 score = 0;
-cross = true;
+cross = true; 
 
 audio = new Audio('music.mp3');
 audiogo = new Audio('gameover.mp3');
 setTimeout(() => {
     audio.play()
 }, 1000);
+
 document.onkeydown = function (e) {
     console.log("Key code is: ", e.keyCode)
     if (e.keyCode == 38) {
@@ -26,7 +27,9 @@ document.onkeydown = function (e) {
         dino.style.left = (dinoX - 112) + "px";
     }
 }
-let gameInterval = true;
+
+let gameRunning = true; 
+
 setInterval(() => {
     dino = document.querySelector('.dino');
     gameOver = document.querySelector('.gameOver');
@@ -40,7 +43,7 @@ setInterval(() => {
 
     offsetX = Math.abs(dx - ox);
     offsetY = Math.abs(dy - oy);
-    // console.log(offsetX, offsetY)
+
     if (offsetX < 73 && offsetY < 52) {
         gameOver.innerHTML = "Game Over - Reload to Play Again"
         obstacle.classList.remove('obstacleAni')
@@ -49,26 +52,20 @@ setInterval(() => {
             audiogo.pause();
             audio.pause();
         }, 1000);
-        gameInterval=false;
-    }
-    else if (offsetX < 145 && cross && gameInterval) {
+        gameRunning = false; 
+    } else if (offsetX < 145 && gameRunning) { // Check gameRunning directly
         score += 1;
         updateScore(score);
-        cross = false;
-        setTimeout(() => {
-            cross = true;
-        }, 1000);
+        // No need for the cross logic here
         setTimeout(() => {
             aniDur = parseFloat(window.getComputedStyle(obstacle, null).getPropertyValue('animation-duration'));
             newDur = aniDur - 0.1;
             obstacle.style.animationDuration = newDur + 's';
             console.log('New animation duration: ', newDur)
         }, 500);
-
     }
-
 }, 10);
 
 function updateScore(score) {
-    scoreCont.innerHTML = "Your Score: " + score
+    scoreCont.innerHTML = "Your Score: " + score;
 }
